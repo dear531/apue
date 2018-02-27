489de1125166ed26be77043074dbe1879f8ed6f0
zhangly <dear531@21cn.com>
2018-02-27 17:22:27 +0800

    path max value

--- uint_max.c
+++ uint_max.c
@@ -6,5 +6,11 @@ int main(int argc, char *argv[])
 	fprintf(stdout, "INT_MAX:\t%d\n", INT_MAX);
 	fprintf(stdout, "UINT_MAX:\t%u\n", UINT_MAX);
 	fprintf(stdout, "0xffffffff:\t%u\n", 0xffffffff);
+	fprintf(stdout, "_POSIX_PATH_MAX:\t%d\n", _POSIX_PATH_MAX);
+	fprintf(stdout, "PATH_MAX:\t%d\n", PATH_MAX);
+	fprintf(stdout, "_POSIX_NAME_MAX:\t%d\n", _POSIX_NAME_MAX);
+	fprintf(stdout, "NAME_MAX:\t%d\n", NAME_MAX);
+	fprintf(stdout, "_POSIX_PATH_MAX:\t%d\n", _POSIX_PATH_MAX);
+	fprintf(stdout, "PATH_MAX:\t%d\n", PATH_MAX);
 	return 0;
 }
