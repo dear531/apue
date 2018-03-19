df29c66614e5efacd090c85a338cb8abf667d862
zhangly <dear531@21cn.com>
2018-03-19 10:50:39 +0800

    separate sys and posix

--- conf.c
+++ conf.c
@@ -11,6 +11,123 @@ static void pr_pathconf(char *, char *, int);
 #else
 #endif
 
+void show_sys_max_value(void)
+{
+	printf("show sys max value\n");
+#ifdef CHAR_BIT
+	printf("CHAR_BIT:%d\n", CHAR_BIT);
+#else
+	printf("CHAR_BIT is not defined\n");
+#endif
+#ifdef CHAR_MAX
+	printf("CHAR_MAX :%d\n", CHAR_MAX);
+#else
+	printf("CHAR_MAX is not defined\n");
+#endif
+
+#ifdef SCHAR_MAX
+	printf("SCHAR_MAX :%d\n", SCHAR_MAX);
+#else
+	printf("SCHAR_MAX is not defined\n");
+#endif
+
+#ifdef SCHAR_MIX
+	printf("SCHAR_MIX :%d\n", SCHAR_MIX);
+#else
+	printf("SCHAR_MIX is not defined\n");
+#endif
+#ifdef UCHAR_MAX
+	printf("UCHAR_MAX:%d\n", UCHAR_MAX);
+#else
+	printf("UCHAR_MAX is not defined\n");
+#endif
+#ifdef UCHAR_MIN
+	printf("UCHAR_MIN :%d\n", UCHAR_MIN);
+#else
+	printf("UCHAR_MIN is not defined\n");
+#endif
+#ifdef MB_LEN_MAX
+	printf("MB_LEN_MAX :%d\n", MB_LEN_MAX);
+#else
+	printf("MB_LEN_MAX is not defined\n");
+#endif
+#ifdef ULLONG_MAX
+	printf("ULLONG_MAX :\t%llu\n", ULLONG_MAX);
+#else
+	printf("ULLONG_MAX is not defined\n");
+#endif
+#ifdef LLONG_MAX
+	printf("LLONG_MAX :\t%lld\n", LLONG_MAX);
+#else
+	printf("LLONG_MAX is not defined\n");
+#endif
+#ifdef FOPEN_MAX
+	printf("FOPEN_MAX :%d\n", FOPEN_MAX);
+#else
+	printf("FOPEN_MAX is not defined\n");
+#endif
+#ifdef TMP_MAX
+	printf("TMP_MAX :%d\n", TMP_MAX);
+#else
+	printf("TMP_MAX is not defined\n");
+#endif
+	printf("=========\n");
+}
+
+void show_posix_value(void)
+{
+	printf("show posix value\n");
+#ifdef _POSIX_CHILD_MAX
+	printf("_POSIX_CHILD_MAX defined to be %d\n", _POSIX_CHILD_MAX);
+#else
+	printf("_POSIX_CHILD_MAX is not defined\n");
+#endif
+	printf("=========\n");
+#ifdef SSIZE_MAX
+	printf("SSIZE_MAX :%ld\n", SSIZE_MAX);
+#else
+	printf("SSIZE_MAX is not defined\n");
+#endif
+#ifdef _POSIX_LINK_MAX
+	printf("_POSIX_LINK_MAX :%d\n", _POSIX_LINK_MAX);
+#else
+	printf("_POSIX_LINK_MAX is not defined\n");
+#endif
+#ifdef _POSIX_HOST_NAME_MAX
+	printf("_POSIX_HOST_NAME_MAX :%d\n", _POSIX_HOST_NAME_MAX);
+#else
+	printf("_POSIX_HOST_NAME_MAX is not defined\n");
+#endif
+#ifdef _POSIX_PATH_MAX
+	printf("_POSIX_PATH_MAX :%d\n", _POSIX_PATH_MAX);
+#else
+	printf("_POSIX_PATH_MAX is not defined\n");
+#endif
+#ifdef _POSIX_NGROUPS_MAX
+	printf("_POSIX_NGROUPS_MAX :%d\n", _POSIX_NGROUPS_MAX);
+#else
+	printf("_POSIX_NGROUPS_MAX is not defined\n");
+#endif
+#ifdef _POSIX_OPEN_MAX
+	printf("_POSIX_OPEN_MAX :%d\n", _POSIX_OPEN_MAX);
+#else
+	printf("_POSIX_OPEN_MAX is not defined\n");
+#endif
+
+#ifdef _POSIX_PIPE_BUF
+	printf("_POSIX_PIPE_BUF :%d\n", _POSIX_PIPE_BUF);
+#else
+	printf("_POSIX_PIPE_BUF is not defined\n");
+#endif
+
+#ifdef _POSIX_STREAM_MAX
+	printf("_POSIX_STREAM_MAX :%d\n", _POSIX_STREAM_MAX);
+#else
+	printf("_POSIX_STREAM_MAX is not defined\n");
+#endif
+}
+
+
 int main(int argc, char *argv[])
 {
 #if 1
@@ -22,12 +139,6 @@ int main(int argc, char *argv[])
 	}
 #endif
 
-#ifdef _POSIX_CHILD_MAX
-	printf("_POSIX_CHILD_MAX defined to be %d\n", _POSIX_CHILD_MAX);
-#else
-	printf("_POSIX_CHILD_MAX is not defined\n");
-#endif
-	printf("=========\n");
 #ifdef ARG_MAX
 	printf("ARG_MAX defined to be %d\n", ARG_AMX+0);
 #else
@@ -107,6 +218,9 @@ int main(int argc, char *argv[])
 	printf("_SC_OPEN_MAX is not defined\n");
 #endif
 	printf("=========\n");
+
+	show_sys_max_value();
+	show_posix_value();
 	return 0;
 }
 
