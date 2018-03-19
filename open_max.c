13c1b0c9f1bed48cda3af0f349c0bdc9416dfa76
zhangly <dear531@21cn.com>
2018-03-19 17:33:41 +0800

    optimized open max

--- open_max.c
+++ open_max.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#if 0
 #ifdef OPEN_MAX
 static long open_max_flag = OPEN_MAX;
 #else
@@ -23,6 +24,24 @@ long open_max(void)
 	}
 	return open_max;
 }
+#else
+#define OPEN_MAX_GUESS 256
+long open_max(void)
+{
+	long open_max = 0;
+#ifdef OPEN_MAX
+	open_max = OPEN_MAX;
+#else
+	if (0 > (open_max = sysconf(_SC_OPEN_MAX)) && 0 == errno) {
+		open_max = OPEN_MAX_GUESS;
+	} else if (0 > open_max && 0 != errno) {
+		fprintf(stderr, "sysconf error :%m\n");
+		exit(EXIT_FAILURE);
+	}
+#endif
+	return open_max;
+}
+#endif
 
 int main(void)
 {
