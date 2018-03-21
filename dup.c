829a561d5c6ca03987b8ac0923ed97d837f2f258
zhangly <dear531@21cn.com>
2018-03-21 16:24:51 +0800

    dup and fcntl for F_DUPFD

---
+++ dup.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int fd1 = -1, fd2 = -1;
+	if (-1 == (fd1 = open("dup.txt", O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR))) {
+		fprintf(stdout, "open error :%m\n");
+		exit(EXIT_FAILURE);
+	}
+#if 1
+	fd2 = 10;
+	if (-1 == fcntl(fd1, F_DUPFD, fd2))
+	{
+		fprintf(stderr, "fcntl error %m\n");
+		exit(EXIT_FAILURE);
+	}
+#else
+	fd2 = dup(fd1);
+	if (-1 == fd2) {
+		fprintf(stderr, "dup error :%m\n");
+		exit(EXIT_FAILURE);
+	}
+#endif
+	fprintf(stdout, "fd1 :%d, fd2 :%d\n", fd1, fd2);
+	write(fd1, "1", strlen("1"));
+	write(fd2, "2", strlen("2"));
+	system("cat dup.txt");
+	if (-1 == fd1) {
+		close(fd1);
+		fd1 = -1;
+	}
+	return 0;
+}
+
