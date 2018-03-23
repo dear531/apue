f1eed02b793ef1d7c81a2ea072fdc6c3cf1016d9
zhangly <dear531@21cn.com>
2018-03-23 14:25:11 +0800

    compare lstat and stat

--- lstat.c
+++ lstat.c
@@ -6,6 +6,18 @@ lstat /etc/tty
 /dev/log S_ISSOCK
 /dev/console S_ISCHR
 link_1.txt S_ISLNK
+
+zhangly@ubuntu:/mnt/data/gitstorage/apue$ ./a.out link_1.txt
+1.txt 
+lstat:
+link_1.txt:S_ISLNK inode:266730
+stat
+link_1.txt:S_ISREG inode:265559
+lstat:
+1.txt:S_ISREG inode:265559
+stat
+1.txt:S_ISREG inode:265559
+
 #endif
 #include <stdio.h>
 #include <sys/types.h>
@@ -37,7 +49,30 @@ int main(int argc, char *argv[])
 			ptr = "S_ISSOCK";
 		else
 			ptr = "unknown file type";
-		fprintf(stdout, "%s %s\n", argv[i], ptr);
+		fprintf(stdout, "lstat:\n%s:%s inode:%ld\n", argv[i], ptr,
+				buf.st_ino);
+
+		if (-1 == stat(argv[i], &buf)) {
+			fprintf(stderr, "lstat %s\n", argv[i]);
+		}
+		if (S_ISREG(buf.st_mode)) 
+			ptr = "S_ISREG";
+		else if (S_ISDIR(buf.st_mode))
+			ptr = "S_ISDIR";
+		else if (S_ISCHR(buf.st_mode))
+			ptr = "S_ISCHR";
+		else if (S_ISBLK(buf.st_mode))
+			ptr = "S_ISBLK";
+		else if (S_ISFIFO(buf.st_mode))
+			ptr = "S_ISFIFO";
+		else if (S_ISLNK(buf.st_mode))
+			ptr = "S_ISLNK";
+		else if (S_ISSOCK(buf.st_mode))
+			ptr = "S_ISSOCK";
+		else
+			ptr = "unknown file type";
+		fprintf(stdout, "stat\n%s:%s inode:%ld\n", argv[i], ptr,
+				buf.st_ino);
 	}
 	return 0;
 }
